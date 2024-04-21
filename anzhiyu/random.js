da25ff01115e4ba10bbbd68c130537b7a44a7913
SukhoiGit <3103846585@qq.com>
2024-04-21 19:13:48 +0800

    Site updated: 2024-04-21 19:13:46

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/47de4be2.html","posts/aa55683f.html","posts/999a8fe.html","posts/c9a945f0.html","posts/fc4c1a2e.html","posts/6bb1d783.html","posts/f6491cfb.html","posts/99345e84.html","posts/7da19302.html","posts/da56859f.html","posts/a42a779d.html","posts/98480eac.html","posts/23a3bd72.html","posts/5729df21.html","posts/5c9cc7e1.html","posts/c943696a.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
