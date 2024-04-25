005d64a28551366c388f0725b28d3f575857d40c
SukhoiGit <3103846585@qq.com>
2024-04-25 21:32:39 +0800

    Site updated: 2024-04-25 21:32:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/999a8fe.html","posts/47de4be2.html","posts/c9a945f0.html","posts/fc4c1a2e.html","posts/aa55683f.html","posts/6bb1d783.html","posts/f6491cfb.html","posts/99345e84.html","posts/7da19302.html","posts/a42a779d.html","posts/da56859f.html","posts/23a3bd72.html","posts/98480eac.html","posts/5c9cc7e1.html","posts/c943696a.html","posts/5729df21.html"];function toRandomPost(){
+var posts=["posts/999a8fe.html","posts/47de4be2.html","posts/c9a945f0.html","posts/fc4c1a2e.html","posts/aa55683f.html","posts/6bb1d783.html","posts/f6491cfb.html","posts/99345e84.html","posts/7da19302.html","posts/a42a779d.html","posts/da56859f.html","posts/23a3bd72.html","posts/98480eac.html","posts/c943696a.html","posts/5729df21.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
