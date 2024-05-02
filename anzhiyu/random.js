46aedd9cb39b3c4dcd6ec0902fc3c1b761602e63
SukhoiGit <3103846585@qq.com>
2024-05-02 09:34:10 +0800

    Site updated: 2024-05-02 09:34:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/c9a945f0.html","posts/6bb1d783.html","posts/98480eac.html","posts/a42a779d.html","posts/9f572938.html","posts/da56859f.html","posts/c943696a.html","posts/47de4be2.html","posts/fc4c1a2e.html","posts/5729df21.html","posts/99345e84.html","posts/23a3bd72.html","posts/999a8fe.html","posts/f6491cfb.html","posts/7da19302.html","posts/aa55683f.html"];function toRandomPost(){
+var posts=["posts/47de4be2.html","posts/999a8fe.html","posts/fc4c1a2e.html","posts/99345e84.html","posts/f6491cfb.html","posts/7da19302.html","posts/5729df21.html","posts/23a3bd72.html","posts/c9a945f0.html","posts/6bb1d783.html","posts/aa55683f.html","posts/98480eac.html","posts/da56859f.html","posts/a42a779d.html","posts/c943696a.html","posts/9f572938.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
